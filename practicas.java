b8a0b9f47a13dbd635de3ecce61992f049541a44
Alexis <franklinfloresra@gmail.com>
2022-10-25 21:27:17 -0600

    Version 2

--- practicas.java
+++ practicas.java
@@ -1,3 +1,9 @@
+//Creacion de variables locales
 public class practicas {
+    String a = "Hola";
+    String b = " ";
+    String c = "Practica";
+    String d = "De";
+    String e = "Concatenar variables";
 
 }
