4d0730c1ae4875f88506a10bf9944e5041f5ad7c
Alexis <franklinfloresra@gmail.com>
2022-10-31 17:26:47 -0600

    Version 1

---
+++ Constante.java
@@ -0,0 +1,11 @@
+public class Constante {
+    public static void main(String[] args) {
+        /* Declaracion de constante */
+        final int valor = 50;
+        /*
+         * Se utiliza la palabra reservada final seguido del tipo mas nombre
+         * y dato asignado
+         */
+        System.out.println("Valor almacenado en la variable: " + valor);
+    }
+}
