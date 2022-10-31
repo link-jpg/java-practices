41307db992ecf88a470d8262edfb6e94e7af488b
Alexis <franklinfloresra@gmail.com>
2022-10-31 17:18:18 -0600

    Version 1

---
+++ Pvariables3.java
@@ -0,0 +1,18 @@
+public class Pvariables3 {
+    /*
+     * Datos Primitivos fueron vistos en la Pvariables2
+     * Datos de tipo no Primitivo
+     */
+    public static void main(String[] args) {
+        Integer valor = 10;// Integer es un tipo de dato no primitivo
+        System.out.println("Valor en Integer: " + valor);
+        // Permite almacenar null
+        Integer valor1 = null;
+        System.out.println("Valor1 en Integer: " + valor1);
+        // Modificacion de cadenas
+        String texto = "Oracion Guardada en texto";
+        System.out.println("Texto es igual a: " + texto);// v1
+        System.out.println(texto);// v2
+
+    }
+}
