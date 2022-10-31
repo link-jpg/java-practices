10f81678200bbb7dfcf20415a5d031b8bbe4c4e7
Alexis <franklinfloresra@gmail.com>
2022-10-31 16:57:50 -0600

    Version 1

---
+++ Pvariables2.java
@@ -0,0 +1,17 @@
+public class Pvariables2 {
+    public static void main(String[] args) {
+        /*
+         * Distintas maneras de asignar un valor a una variable
+         */
+        // Version 1
+        byte valor; // Se escribe el tipo de dato y el nombre de la variable
+        valor = 10; // se escribe la variable mas el dato que queremos asignarle
+        System.out.println("Dato almacenado en valor: " + valor);// + sirve para concatenar
+        // Fin version 1
+        // Version 2
+        byte valor1 = 10;// El tipo de dato mas el nombre mas = el valor a asignarle
+        System.out.println("Dato almacenado en valor1: " + valor1);
+        // Fin version 2
+
+    }
+}
