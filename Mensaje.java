32bd4f4bc525d6b62fd51bdca0e440491812b08a
Alexis <franklinfloresra@gmail.com>
2022-10-31 16:41:16 -0600

    Version 1

---
+++ Mensaje.java
@@ -0,0 +1,6 @@
+public class Mensaje {
+    public static void main(String[] args) {
+        System.out.println("Hola, Iniciando en el mundo de la programacion");
+        System.out.println("Brevemente ire actualizando estos repositorios");
+    }
+}
